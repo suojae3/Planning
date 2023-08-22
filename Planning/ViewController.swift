174b4ebf00aadb476f8b5cd289e8166162c822d5
suojae <suojae3@gmail.com>
2023-08-22 22:24:58 +0900

    Initial Commit

---
+++ Planning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planning
+//
+//  Created by ㅣ on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
